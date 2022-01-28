ae86a25d2318d0782fbfbe933efdc8475c108b0b
Azamat <axrorxojayevazamat@gmail.com>
2022-01-28 09:06:52 +0500

    create models from db mysql

--- CallCenterCRM/Program.cs
+++ CallCenterCRM/Program.cs
@@ -1,8 +1,10 @@
+using CallCenterCRM.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<CallcentercrmContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
